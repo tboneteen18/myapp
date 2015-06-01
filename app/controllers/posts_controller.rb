1375bec30a28e0a6f54dc441e1bd0ed5e6b58402
Tboneteen18 <tboneteen18@gmail.com>
2015-06-01 15:30:23 -0600

    added a new page and better functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,12 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def show
+   @post = Post.find(params[:id])
+  end
+
   def new
     @post = Post.new
   end
@@ -15,6 +23,22 @@ class PostsController < ApplicationController
 
   def edit
     @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to posts_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to posts_path
   end
 
   private
